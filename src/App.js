b6fd59781b9d37debf0a0e86e2685da08c663a91
XimenaTania <xime.tania17@gmail.com>
2022-09-09 09:51:17 -0400

    pulsadorPrimeraParte

--- src/App.js
+++ src/App.js
@@ -1,48 +1,46 @@
+
 import './App.css';
 import logo from './imagens/logo.png'
 import Boton from './componentes/boton'
 import Contador from './componentes/contador';
-import { useState } from 'react'
+import{ useState} from 'react'
 
 function App() {
-    const [numClics, setNumClics] = useState(0);
+  const [numClics,setNumClics] = useState(0);
 
 
-    const manejarClic = () => {
-        setNumClics(numClics + 1);
+const manejarClic = () =>{
+ setNumClics (numClics + 1);
 
-    }
-    const reiniciarContador = () => {
-        setNumClics(0);
-    }
+}
+const reiniciarContador =() =>{
+  setNumClics(0);
+}
 
 
-    return ( <
-        div className = "App" >
-
-        <
-        div className = 'losAndesLogoContenedor' >
-        <
-        img className = 'LosAndesLogo'
-        src = { logo }
-        alt = 'LogoLosAndes' / >
-        <
-        /div> <
-        div className = 'contenerdorPrincipal' >
-        <
-        Contador numClics = { numClics }
-        /> <
-        Boton texto = 'Clic'
-        esBotonclic = { true }
-        manejarClic = { manejarClic }
-        /> <
-        Boton texto = 'Reiniciar'
-        esBotonclic = { false }
-        manejarClic = { reiniciarContador }
-        /> <
-        /div> <
-        /div>
-    );
+  return (
+    <div className="App">
+    
+      <div className='losAndesLogoContenedor'>
+        <img 
+        className='LosAndesLogo'
+        src={logo}
+        alt='LogoLosAndes'/>
+      </div>
+      <div className='contenerdorPrincipal'>
+        <Contador numClics={numClics} />
+        <Boton
+        texto='Clic'
+        esBotonclic={true}
+        manejarClic={manejarClic}/>
+        <Boton
+        texto='Reiniciar'
+        esBotonclic={false}
+        manejarClic={reiniciarContador}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
